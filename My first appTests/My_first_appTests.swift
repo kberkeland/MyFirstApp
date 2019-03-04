e018b037f8b32ed2d052e928b647e169ab2ab298
Kye Berkeland <kyeberkeland@yahoo.com>
2019-03-04 09:02:58 -0600

    Initial Commit

---
+++ My first appTests/My_first_appTests.swift
@@ -0,0 +1,34 @@
+//
+//  My_first_appTests.swift
+//  My first appTests
+//
+//  Created by Kye Berkeland on 3/4/19.
+//  Copyright © 2019 Kye Berkeland. All rights reserved.
+//
+
+import XCTest
+@testable import My_first_app
+
+class My_first_appTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
